84f0f65c902d1aa4836f08629bb07e1f91526e42
shubhamjain <shubhamjainnie@gmail.com>
2024-09-22 00:13:20 +0530

    First java commit

---
+++ src/main/java/com/shubham/cruddemo/CruddemoApplication.java
@@ -0,0 +1,30 @@
+package com.shubham.cruddemo;
+
+import com.shubham.cruddemo.dao.StudentDAO;
+import com.shubham.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student...");
+		Student student = new Student("Shiv","Shakti","shiva@gmail.com");
+		studentDAO.save(student);
+		System.out.println("Student created");
+	}
+}
---
+++ src/main/java/com/shubham/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.shubham.cruddemo.dao;
+
+import com.shubham.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
